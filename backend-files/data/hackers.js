e9e252576bab26a7e88059ba4edb0f9a072ec2e7
WebDeke <guruweb10@gmail.com>
2022-01-29 07:35:00 -0800

    just learned some neat stuff man, hype

---
+++ backend-files/data/hackers.js
@@ -0,0 +1,28 @@
+const hackers = [
+  {
+    _id: "1",
+    first_name: "Ameya",
+    last_name: "Singh",
+    time_zone: "America/Los_Angeles",
+    email: "thewebdeke@gmail.com",
+    level_of_ed: "High School",
+    phone: "+1 (908) 567-3244",
+    github: "https://github.com/webdeke",
+    status: "Looking for teammates to join existing team",
+    skills: ["JavaScript", "HTML", "CSS", "React", "Node.js", "Express"],
+  },
+  {
+    _id: "2",
+    first_name: "Anthony",
+    last_name: "Duan",
+    time_zone: "America/New_York",
+    email: "ad@gmail.com",
+    level_of_ed: "High School",
+    phone: "+1 (908) 567-3244",
+    github: "https://github.com/duanster69",
+    status: "Looking for teammates to join existing team",
+    skills: ["JavaScript", "Java"],
+  },
+];
+
+module.exports = hackers;
--- backend-files/server.js
+++ backend-files/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const hackers = require("./data/hackers.js");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("The API is running.");
 });
 
-app.listen(3069, console.log("Server is running on port 3069"));
+app.get("/api/hackers", (req, res) => {
+  res.json(hackers);
+});
+
+app.get("/api/hackers/:id", (req, res) => {
+  const hacker = hackers.find((hacker) => hacker._id === req.params.id);
+
+  res.send(hacker);
+});
+
+const PORT = process.env.PORT || 3069;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
